28428a56c559f34925e0313a91f053e1061f7fb0
Jaak Randmets <jaak.randmets@cyber.ee>
2017-02-28 18:11:34 +0200

    More work on Z3 bridge.
    Turns out that Z3 supports strings.

--- src/Z3Bridge.hs
+++ src/Z3Bridge.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE LambdaCase #-}
 
-module Z3Bridge where
+module Z3Bridge (
+  dbFromCatalogUpdates,
+  generateZ3)
+  where
 
 import Control.Arrow
 import Data.Char
 import Data.List
 import Data.Map (Map)
-import Data.Text (unpack)
+import Data.Text (unpack, pack)
 import Database.HsSqlPpp.Catalog
 import Database.HsSqlPpp.Syntax
 import Text.Printf
@@ -14,10 +17,15 @@ import Text.Printf
 import qualified Data.Map as Map
 
 import Logging
+import SelectQuery
 
 --------------------------------------
 -- Some generic Z3 code gen helpers --
 --------------------------------------
+
+-- function concat (or something)
+fcat :: [a -> a] -> a -> a
+fcat = foldr (.) id
 
 showP :: ShowS -> ShowS
 showP s = showChar '(' . s . showChar ')'
@@ -28,28 +36,44 @@ space = showChar ' '
 nl :: ShowS
 nl = showChar '\n'
 
-z3Assert :: ShowS -> ShowS
-z3Assert s = showP (showString "assert" . space . s) . nl
-
 z3Push :: ShowS
-z3Push = showString "(push)" . nl
+z3Push = showString "(push)\n"
 
 z3Pop :: ShowS
-z3Pop = showString "(pop)" . nl
+z3Pop = showString "(pop)\n"
+
+z3CheckSat :: ShowS
+z3CheckSat = showString "(check-sat)\n"
 
 z3Isolate :: ShowS -> ShowS
 z3Isolate s = z3Push . s . z3Pop
 
+z3Assert :: ShowS -> ShowS
+z3Assert s = showP (showString "assert" . space . s) . nl
+
 z3DeclareConst :: ShowS -> ShowS -> ShowS
 z3DeclareConst var ty = showP (showString "declare-const" . space . var . space . ty) . nl
+
+------------------------------------
+-- Information about the database --
+------------------------------------
+
+-- TODO: move to Schema.hs
+
+type TableInfo = [(CatName, CatName)]
+type DbSchema = Map CatName TableInfo
+
+dbTables :: DbSchema -> [CatName]
+dbTables = map fst . Map.toList
+
+dbFromCatalogUpdates :: [CatalogUpdate] -> DbSchema
+dbFromCatalogUpdates us = Map.fromList [(n, map (second catName) cols) | CatCreateTable (_, n) cols <- us]
 
 ---------------------------
 -- Generate Z3 statement --
 ---------------------------
 
-type TableInfo = [(CatName, CatName)]
-type DbSchema = Map CatName TableInfo
-
+-- TODO: partial
 -- ^ Generate Z3 code to verify if query is <= 1 sensitive
 generateZ3 :: [[NameComponent]]  -- ^ uniques
            -> [NameComponent]    -- ^ non-nulls
@@ -57,11 +81,27 @@ generateZ3 :: [[NameComponent]]  -- ^ uniques
            -> DbSchema           -- ^ information about the database
            -> QueryExpr          -- ^ query
            -> ShowS
-generateZ3 uniques nonnulls es q = undefined
+generateZ3 _ _ _ schema query =
+  fcat $ map go uniqueJoinTables
+  where
+    joinTables = map (nameToCatName.getName) $ extractJoinTables query
+
+    uniqueJoinTables = nub joinTables
+
+    getName (Tref _ n) = n
+
+    go fixedTable =
+      z3Push .
+      genDecls schema fixedTable .
+      z3CheckSat .
+      z3Pop
+
+nameToCatName :: Name -> CatName
+nameToCatName n = pack $ genName n ""
 
 genName :: Name -> ShowS
 genName (AntiName n) = showString n
-genName (Name _ ns) = foldr (.) id $ intersperse (showString ".") $ map genNameComponent ns
+genName (Name _ ns) = fcat $ intersperse (showString "-") $ map genNameComponent ns
 
 -- TODO: "IS" and "IS NOT" dont work the same way as "=" and "!="
 genOp :: Name -> ShowS
@@ -84,6 +124,8 @@ genType n = showString $ case unpack n of
   "int8"    -> "Int"
   "integer" -> "Int"
   "int"     -> "Int"
+  "bool"    -> "Bool"
+  "text"    -> "String"
   n'        -> ice $ printf "Invalid type \"%s\"." n'
 
 genNameComponent :: NameComponent -> ShowS
@@ -98,7 +140,7 @@ genColNamePrefix :: CatName -> (CatName, CatName) -> ShowS
 genColNamePrefix tbl (row, _) = genCatName tbl . showChar '-' . genCatName row
 
 genDecls :: DbSchema -> CatName -> ShowS
-genDecls schema fixed = foldr (.) id $ concatMap go $ Map.toList schema
+genDecls schema fixed = fcat $ concatMap go $ Map.toList schema
   where
     go (tbl, cols)
       | tbl == fixed = map (goFixed tbl) cols
@@ -125,6 +167,7 @@ genScalarExpr = \case
   Identifier _ n -> genName n
   NumberLit _ s -> showString s
   BooleanLit _ b -> shows b
+  StringLit _ s -> shows s
   Parens _ e -> genScalarExpr e
   PrefixOp _ op e ->
     showP            $

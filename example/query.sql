a0f8609392d0eb8aef0218ba372875cb57700233
Jaak Randmets <jaak.randmets@cyber.ee>
2017-03-27 14:32:54 +0300

    Add example query and schema.

---
+++ example/query.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT * FROM t, t2, t3 where t.id = t2.id AND age > 0;
---
+++ example/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE t (
+    id int4 PRIMARY KEY,
+    foo text NOT NULL,
+    bar int4 NOT NULL
+);
+
+CREATE TABLE t2 (
+    id int4 NOT NULL,
+    name text NOT NULL,
+    age int2 NOT NULL
+);
+
+CREATE TABLE t3 (
+    id int4 PRIMARY KEY
+);

2cd6530d135789ec9d3e106c2634828e21752dd6
Martin Pettai <martinp534@users.noreply.github.com>
2018-05-07 20:37:52 +0300

    BanachQ: add support for sending queries automatically to the PostgreSQL database.

--- banach/ProgramOptions.hs
+++ banach/ProgramOptions.hs
@@ -10,6 +10,9 @@ data ProgramOptions
     inputFp          :: FilePath,
     alternative      :: Bool,
     generateQueries  :: Bool,
+    dbSensitivity    :: Bool,
+    dbCreateTables   :: Bool,
+    dbConnString     :: String,
     getEpsilon       :: Double,
     getBeta          :: Maybe Double
   }
@@ -19,6 +22,9 @@ programArgs = ProgramOptions
   <$> strArgument (metavar "INPUT" <> help "Input file")
   <*> switch (short 'a' <> long "alternative" <> hidden <> help "Use alternative input and output format")
   <*> switch (short 'Q' <> long "queries" <> hidden <> help "Generate SQL queries for computing sensitivity instead of actually computing it")
+  <*> switch (short 'D' <> long "db-sensitivity" <> hidden <> help "Send the generated query for computing sensitivity automatically to the database")
+  <*> switch (long "db-create-tables" <> hidden <> help "Create the required tables in the database using the data in input files")
+  <*> strOption (short 'C' <> long "db-connection" <> value "dbname=banach" <> help "Specify database connection string")
   <*> option auto (short 'e' <> long "epsilon" <> value 1.0 <> help "Specify epsilon (default is 1)")
   <*> optional (option auto (short 'B' <> long "beta" <> help "Specify beta (default is to choose automatically)"))
 

9017ee980b9bb57a04debbce5c4c036743a4726e
Alisa <alisa@cyber.ee>
2018-05-29 15:20:29 +0300

    Added an old-formatted demo query that can be run with old version.

---
+++ banach/demo_old.sql
@@ -0,0 +1,37 @@
+reachable_ports =
+SELECT
+    port.port_id AS port_id,
+    ((ship.latitude - port.latitude) ^ 2 + (ship.longitude - port.longitude) ^ 2) ^ 0.5 / ship.max_speed AS arrival
+from port, ship, parameters
+WHERE
+    ship.name = parameters.shipname
+
+feasible_ports =
+SELECT
+    port.port_id AS port_id
+FROM reachable_ports, port, ship, parameters
+WHERE
+    reachable_ports.port_id = port.port_id
+    AND port.available
+    AND port.harbordepth >= ship.draft
+    AND port.offloadcapacity >= ship.cargo
+    AND ship.name = parameters.shipname
+
+ship_arrival_to_port =
+SELECT
+    min(rport.arrival)
+FROM
+    reachable_ports AS rport,
+    feasible_ports AS fport,
+    port, slot, berth, ship, parameters
+WHERE
+    port.port_id = fport.port_id
+    and port.port_id = rport.port_id
+    AND port.port_id = berth.port_id
+    AND slot.port_id = berth.port_id
+    AND slot.berth_id = berth.berth_id
+    AND ship.name = parameters.shipname
+    AND berth.berthlength >= ship.length
+    AND slot.slotstart <= parameters.deadline
+    AND slot.slotstart + port.offloadtime <= slot.slotend
+;
---
+++ banach/demo_schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE parameters (  param_id INT8 PRIMARY KEY, deadline INT8, shipname TEXT);
+create table ship (  ship_id INT8 primary key,  name TEXT,  cargo INT8,  latitude INT8,  longitude INT8,  length INT8,  draft INT8,  max_speed INT8);
+create table port (  port_id INT8 primary key,  name TEXT,  latitude INT8,  longitude INT8,  offloadcapacity INT8,   offloadtime INT8,  harbordepth INT8,  available Bool);
+create table berth (  port_id INT8,  berth_id INT8,  berthlength INT8);
+CREATE TABLE slot (  port_id INT8, berth_id INT8, slot_id INT8, ship_id INT8, slotstart INT8, slotend INT8);
+

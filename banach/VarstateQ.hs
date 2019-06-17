85ea52d2642de2df5ab5f2fc56b8b25a03b079ca
Alisa <alisa@cyber.ee>
2019-06-17 14:55:26 +0300

    Forgot to commit VarstateQ.hs the previous time.

---
+++ banach/VarstateQ.hs
@@ -0,0 +1,22 @@
+module VarstateQ where
+
+import qualified Data.Map as M
+import ErrorMsg
+
+---------------------------------------------------
+---- variable state and policy data structures ----
+---------------------------------------------------
+
+data VState a
+  = Exact | None
+  | Approx a                       | ApproxWrtLp Double a           | ApproxWrtLinf a
+  | IntSubSet   [Int]              | SubSet   [String]              | Range   a a                | Total Int
+  | IntSubSetUn [Int]              | SubSetUn [String]              | RangeUn a a                | TotalUn Int
+  | IntSubSetPr (M.Map Int Double) | SubSetPr (M.Map String Double) | RangePr a (M.Map a Double)
+  deriving (Show)
+
+
+type VarState = VState Double
+
+getUb :: Double -> M.Map Double Double -> Double
+getUb lb mp = foldr max lb (M.keys mp)
